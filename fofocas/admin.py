41ee037e93479a1f876a8a7140ab931edcdf593e
Eduarda Campos <eduarda.prc@gmail.com>
2025-07-24 19:34:44 -0300

    Register Fofoca model in admin with custom admin interface

--- fofocas/admin.py
+++ fofocas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fofoca
 
-# Register your models here.
+@admin.register(Fofoca)
+class FofocaAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'bairro', 'categoria', 'votos_confirmo', 'votos_mentira']
